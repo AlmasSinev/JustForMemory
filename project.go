fe9a47b97714f56e87541340d8948b38d6db9abd
Алмас Синев <26almassinev@gmail.com>
2021-04-06 02:07:25 +0300

    First pages on Go lang

--- project.go
+++ project.go
@@ -1,8 +1,21 @@
 package main
 
-import "fmt"
+import ("fmt"
+        "net/http")
+
+func home_page(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "<h1>Go go go go!</h1>")
+}
+func help_page(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "It is a help page")
+}
+
+func handleReguest() {
+  http.HandleFunc("/", home_page)
+  http.HandleFunc("/help/", help_page)
+  http.ListenAndServe(":8080", nil)
+}
 
 func main() {
-  fmt.Println("Hello World!")
-  fmt.Println("It's the Go lang!!!")
+  handleReguest()
 }
