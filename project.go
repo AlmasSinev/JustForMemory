df0c133b4fac732d2c6475fd67ed8842f40953b4
Алмас Синев <26almassinev@gmail.com>
2021-04-06 12:42:34 +0300

    First site in Go lang

--- project.go
+++ project.go
@@ -1,27 +1,29 @@
 package main
 
-import ("fmt"
-        "net/http")
+import ("fmt"; "net/http"; "html/template")
 
 type User struct {
-    name string
-    age uint16
-    money int16
-    avg_grades, happiness float64
+    Name string
+    Age uint16
+    Money int16
+    Avg_grades, Happiness float64
+    Hobbies []string
 }
 
 func (u User) getAllInfo() string {
   return fmt.Sprintf("User name is %s. " +
-    "He is %d and he has money equal: %d", u.name, u.age, u.money)
+    "He is %d and he has money equal: %d", u.Name, u.Age, u.Money)
 }
 func (u *User) setNewName(newName string) {
-  u.name = newName
+  u.Name = newName
 }
 
 func home_page(w http.ResponseWriter, r *http.Request) {
-  stepan := User{"Stepan", 21, 5000, 4.5, 0.9}
-  stepan.setNewName("Stefan")
-  fmt.Fprintf(w, stepan.getAllInfo())
+  ildar := User{"Ildar", 21, 5000, 4.5, 0.9, []string{"Programming", "Music", "Deutch language"}}
+  //stepan.setNewName("Stefan")
+  //fmt.Fprintf(w, stepan.getAllInfo())
+  tmpl, _ := template.ParseFiles("templates/home_page.html")
+  tmpl.Execute(w, ildar)
 }
 func help_page(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, "It is a help page")
@@ -34,9 +36,6 @@ func handleReguest() {
 }
 
 func main() {
-
   //bob := User{name: "Bob", age: 25, money: -50, avg_grades: 4.2, happiness: 0.6}
-
-
   handleReguest()
 }
